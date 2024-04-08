c28311cd012b2466d93709791f0a753a6d86311a
rjindavidson <davidsonryan276@gmail.com>
2024-04-08 11:24:15 -0700

    navbar options collapse to menu

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,34 @@ import HeroSection from "@/components/hero";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col p-24 bg-slate-800">
+    <main className="flex min-h-screen flex-col p-4 pt-24 bg-slate-800 md:p-24">
       <HeroSection title="I'm Ryan Davidson"/>
-      
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae eaque voluptates molestias eum aperiam aliquid dicta veritatis et neque, distinctio vitae dolorem temporibus repudiandae quia, quasi ipsum impedit at asperiores.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae eaque voluptates molestias eum aperiam aliquid dicta veritatis et neque, distinctio vitae dolorem temporibus repudiandae quia, quasi ipsum impedit at asperiores.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae eaque voluptates molestias eum aperiam aliquid dicta veritatis et neque, distinctio vitae dolorem temporibus repudiandae quia, quasi ipsum impedit at asperiores.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae eaque voluptates molestias eum aperiam aliquid dicta veritatis et neque, distinctio vitae dolorem temporibus repudiandae quia, quasi ipsum impedit at asperiores.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae eaque voluptates molestias eum aperiam aliquid dicta veritatis et neque, distinctio vitae dolorem temporibus repudiandae quia, quasi ipsum impedit at asperiores.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae eaque voluptates molestias eum aperiam aliquid dicta veritatis et neque, distinctio vitae dolorem temporibus repudiandae quia, quasi ipsum impedit at asperiores.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae eaque voluptates molestias eum aperiam aliquid dicta veritatis et neque, distinctio vitae dolorem temporibus repudiandae quia, quasi ipsum impedit at asperiores.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae eaque voluptates molestias eum aperiam aliquid dicta veritatis et neque, distinctio vitae dolorem temporibus repudiandae quia, quasi ipsum impedit at asperiores.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae eaque voluptates molestias eum aperiam aliquid dicta veritatis et neque, distinctio vitae dolorem temporibus repudiandae quia, quasi ipsum impedit at asperiores.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae eaque voluptates molestias eum aperiam aliquid dicta veritatis et neque, distinctio vitae dolorem temporibus repudiandae quia, quasi ipsum impedit at asperiores.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae eaque voluptates molestias eum aperiam aliquid dicta veritatis et neque, distinctio vitae dolorem temporibus repudiandae quia, quasi ipsum impedit at asperiores.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae eaque voluptates molestias eum aperiam aliquid dicta veritatis et neque, distinctio vitae dolorem temporibus repudiandae quia, quasi ipsum impedit at asperiores.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae eaque voluptates molestias eum aperiam aliquid dicta veritatis et neque, distinctio vitae dolorem temporibus repudiandae quia, quasi ipsum impedit at asperiores.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae eaque voluptates molestias eum aperiam aliquid dicta veritatis et neque, distinctio vitae dolorem temporibus repudiandae quia, quasi ipsum impedit at asperiores.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae eaque voluptates molestias eum aperiam aliquid dicta veritatis et neque, distinctio vitae dolorem temporibus repudiandae quia, quasi ipsum impedit at asperiores.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae eaque voluptates molestias eum aperiam aliquid dicta veritatis et neque, distinctio vitae dolorem temporibus repudiandae quia, quasi ipsum impedit at asperiores.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae eaque voluptates molestias eum aperiam aliquid dicta veritatis et neque, distinctio vitae dolorem temporibus repudiandae quia, quasi ipsum impedit at asperiores.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae eaque voluptates molestias eum aperiam aliquid dicta veritatis et neque, distinctio vitae dolorem temporibus repudiandae quia, quasi ipsum impedit at asperiores.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae eaque voluptates molestias eum aperiam aliquid dicta veritatis et neque, distinctio vitae dolorem temporibus repudiandae quia, quasi ipsum impedit at asperiores.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae eaque voluptates molestias eum aperiam aliquid dicta veritatis et neque, distinctio vitae dolorem temporibus repudiandae quia, quasi ipsum impedit at asperiores.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae eaque voluptates molestias eum aperiam aliquid dicta veritatis et neque, distinctio vitae dolorem temporibus repudiandae quia, quasi ipsum impedit at asperiores.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae eaque voluptates molestias eum aperiam aliquid dicta veritatis et neque, distinctio vitae dolorem temporibus repudiandae quia, quasi ipsum impedit at asperiores.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae eaque voluptates molestias eum aperiam aliquid dicta veritatis et neque, distinctio vitae dolorem temporibus repudiandae quia, quasi ipsum impedit at asperiores.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae eaque voluptates molestias eum aperiam aliquid dicta veritatis et neque, distinctio vitae dolorem temporibus repudiandae quia, quasi ipsum impedit at asperiores.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae eaque voluptates molestias eum aperiam aliquid dicta veritatis et neque, distinctio vitae dolorem temporibus repudiandae quia, quasi ipsum impedit at asperiores.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae eaque voluptates molestias eum aperiam aliquid dicta veritatis et neque, distinctio vitae dolorem temporibus repudiandae quia, quasi ipsum impedit at asperiores.
     </main>
   );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,13 @@
+"use client"
+
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { useState } from "react";
+import { MdMenu, MdClose } from "react-icons/md";
+
 
 const Navbar = () => {
+    const [navbar, setNavbar] = useState(false);
     return (
         <nav className="w-full fixed bg-secondary bg-slate-900 text-white flex justify-between items-center p-4 rounded-xl shadow-sm">
             <div>
@@ -12,24 +18,31 @@ const Navbar = () => {
                 </Button>
             </div>
             <div>
-                <Button variant="ghost">
-                    <Link href="#home">
-                        Home
-                    </Link>
-                </Button>
-                <Button variant="ghost">
-                    <Link href="#projects">
-                        Projects
-                    </Link>
-                </Button>
-                <Button variant="ghost">
-                    <Link href="#">
-                        Photos
-                    </Link>
-                </Button>
-                <Button variant="ghost">
-                    Contact
-                </Button>
+                <div className="md:hidden">
+                    <button onClick={() => setNavbar(!navbar)}>
+                        {navbar ? <MdClose size={30} /> : <MdMenu size={30}/>}
+                    </button>
+                </div>
+                <div className={`${navbar ? "block" : "hidden"} md:block`}>
+                    <Button variant="ghost">
+                        <Link href="#home">
+                            Home
+                        </Link>
+                    </Button>
+                    <Button variant="ghost">
+                        <Link href="#projects">
+                            Projects
+                        </Link>
+                    </Button>
+                    <Button variant="ghost">
+                        <Link href="#">
+                            Photos
+                        </Link>
+                    </Button>
+                    <Button variant="ghost">
+                        Contact
+                    </Button>
+                </div>
             </div>
         </nav>
     )
